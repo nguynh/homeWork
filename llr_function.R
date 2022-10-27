cac01f0655a8580de7e391c1aeb8637aa5db34c5
Nam Nguyen <nguynh@iu.edu>
2022-10-27 15:55:37 -0400

    mod code 1

--- llr_function.R
+++ llr_function.R
@@ -6,17 +6,19 @@ llr <- function(x,y,z,omega) {
 }
 
 compute_f_hat <- function(z, x, y, omega) {
-  Wz <- make_weight_matrix(z, x, omega)
+  W <- make_weight_matrix(z, x, omega)
   X <- make_predictor_matrix(x)
-  f_hat <- c(1,z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  y <- matrix(y)
+  WX <- apply(X, 2, function(t) W*t)
+  Wy <- apply(y, 2, function(t) W*t)
+  f_hat <- c(1,z) %*% solve(t(X) %*% WX) %*% t(X) %*% Wy
   return(f_hat)
 }
 
 make_weight_matrix <- function(z, x, omega) {
   r <- abs(x - z) / omega
   w <- ifelse(abs(r) < 1, (1-abs(r)^3)^3, 0)
-  W <- diag(w)
-  return(W)
+  return(w)
 }
 
 make_predictor_matrix <- function(x) {
